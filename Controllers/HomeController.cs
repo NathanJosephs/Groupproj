74809906fe41f4031b27d1a15cba5b3d1a4cbf1d
NathanJosephs <nathanjosephshawera@gmail.com>
2021-09-28 15:46:08 +1300

    deleted old stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,7 +39,7 @@ namespace Groupproj.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
