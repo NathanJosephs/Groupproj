75f00013058b29ce95a3e379a2a1737cdf3f88aa
Luke Robinson <1931689@studentmail.ucol.ac.nz>
2021-09-28 15:05:34 +1300

    added link for sprint retrospective 1

--- Views/Accounts/Login.cshtml.cs
+++ Views/Accounts/Login.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using TekkenClub.Models;
+using Groupproj.Models;
 
 namespace TekkenClub.Pages.Account
 {
