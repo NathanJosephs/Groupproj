8a38965793d86257ae86d0826668e5c1580f0c2c
Carl Ellis <carlhlellis89@gmail.com>
2021-10-19 14:07:01 +1300

    Done MVC basic layout

---
+++ Controllers/TimetablesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Groupproj.Controllers
+{
+    public class TimetablesController : Controller
+    {
+        public IActionResult ClassTimetable()
+        {
+            return View();
+        }
+
+
+        public IActionResult ExamTimetable()
+        {
+            return View();
+        }
+    }
+}
